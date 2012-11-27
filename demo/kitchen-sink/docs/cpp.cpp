60130d690ae40ffc021308c184193e4fbac8b752
Garen Torikian <gjtorikian@gmail.com>
2012-11-26 16:37:39 -0800

    Stash

--- demo/kitchen-sink/docs/cpp.cpp
+++ demo/kitchen-sink/docs/cpp.cpp
@@ -1,13 +1,40 @@
-// compound assignment operators
+/* Hopefully this Java program should demonstrate how elastic tabstops work.	*/
+/* Try inserting and deleting different parts of the text and watch as the tabstops move.	*/
+/* If you like this, please ask the writers of your text editor to implement it.	*/
 
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-int main ()
+struct ipc_perm
 {
-    int a, b=3; /* foobar */
-    a = b;
-    a+=2; // equivalent to a=a+2
-    cout << a;
-    return 0;
+	key_t	key;
+	ushort	uid;	/* owner euid and egid	*/
+	ushort	gid;	/* group id	*/
+	ushort	cuid;	/* creator euid and egid	*/
+	cell-missing		/* for test purposes	*/
+	ushort	mode;	/* access modes	*/
+	ushort	seq;	/* sequence number	*/
+};
+
+int someDemoCode(	int fred,
+	int wilma)
+{
+	x();	/* try making	*/
+	printf("hello!\n");	/* this comment	*/
+	doSomethingComplicated();	/* a bit longer	*/
+	for (i = start; i < end; ++i)
+	{
+		if (isPrime(i))
+		{
+			++numPrimes;
+		}
+	}
+	return numPrimes;
 }
+
+---- and now for something completely different: a table ----
+
+Title	Author	Publisher	Year
+Generation X	Douglas Coupland	Abacus	1995
+Informagic	Jean-Pierre Petit	John Murray Ltd	1982
+The Cyberiad	Stanislaw Lem	Harcourt Publishers Ltd	1985
+The Selfish Gene	Richard Dawkins	Oxford University Press	2006
