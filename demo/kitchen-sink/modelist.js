4cbde4772675e52af74932f8170f09573427430c
nightwing <amirjanyan@gmail.com>
2013-01-03 01:35:21 +0400

    add mode for textmate snippets

--- demo/kitchen-sink/modelist.js
+++ demo/kitchen-sink/modelist.js
@@ -90,6 +90,7 @@ var modesByName = {
     tex:        ["Tex"          , "tex"],
     text:       ["Text"         , "txt"],
     textile:    ["Textile"      , "textile"],
+    tm_snippet: ["tmSnippet"    , "tmSnippet"],
     typescript: ["Typescript"   , "typescript|ts|str"],
     vbscript:   ["VBScript"     , "vbs"],
     xml:        ["XML"          , "xml|rdf|rss|wsdl|xslt|atom|mathml|mml|xul|xbl"],
